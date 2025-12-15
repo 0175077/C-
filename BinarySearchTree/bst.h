f3f65c350df102afa2d8da68ede7017ecdc1c4ee
0175077 <0175077@naver.com>
2025-12-15 15:42:23 +0900

    bst

--- BinarySearchTree/bst.h
+++ BinarySearchTree/bst.h
@@ -18,9 +18,12 @@ private:
 
 	void printNode(S_NODE* pNode);
 	S_NODE* createNode(int nData);
+	S_NODE* insertNodef(S_NODE* pNode, int nData);
+
 public:
 	C_BST() = default;
 	bool insert(int nData);
-
+	void erase(int nData);
+	void eraseF(int nData);
 	void print();
 };
